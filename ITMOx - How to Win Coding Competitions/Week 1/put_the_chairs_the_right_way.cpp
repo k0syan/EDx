3cf26c9951480a6fc6cdcdb02e4eaf2ca7aa3c63
shahenkosyan <shahen.qosyan@gmail.com>
2017-03-16 18:12:36 +0400

    Put the Chairs the Right Way - Accepted

--- ITMOx - How to Win Coding Competitions/Week 1/put_the_chairs_the_right_way.cpp
+++ ITMOx - How to Win Coding Competitions/Week 1/put_the_chairs_the_right_way.cpp
@@ -12,6 +12,13 @@
 #include <iomanip>
 
 int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  
+  double p = a / 2.0 + b / 2.0 + c / 2.0;
+  p = p / 3.0;
+  
+  cout << std::setprecision(8) << p << '\n';
   
   return 0;
 }
