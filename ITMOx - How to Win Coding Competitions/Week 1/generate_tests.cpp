caf0e9941f9b506e982ee6dcf7963b7cf2ca7c78
shahenkosyan <shahen.qosyan@gmail.com>
2017-03-26 22:09:36 +0400

    Generate Tests init

---
+++ ITMOx - How to Win Coding Competitions/Week 1/generate_tests.cpp
@@ -0,0 +1,18 @@
+#ifdef JUDGE
+  #include <fstream>
+  std::ifstream cin("input.txt");
+  std::ofstream cout("output.txt");
+#else
+  #include <iostream>
+  using std::cin;
+  using std::cout;
+#endif
+
+#include <vector>
+
+typedef unsigned long long ull;
+
+int main() {
+  
+  return 0;
+}
