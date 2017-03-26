1fa0a1b92a458cee4be7a26491baad36702473e6
shahenkosyan <shahen.qosyan@gmail.com>
2017-03-26 23:45:16 +0400

    Win The Competition Accepted

--- ITMOx - How to Win Coding Competitions/Week 1/win_the_competition.cpp
+++ ITMOx - How to Win Coding Competitions/Week 1/win_the_competition.cpp
@@ -9,8 +9,30 @@
 #endif
 
 #include <vector>
+#include <algorithm>
 
 int main() {
+  int n, i;
+  cin >> n;
+  
+  std::vector<int> times(n);
+  for (i = 0; i < n; ++i) {
+    cin >> times[i];
+  }
+  
+  std::sort(begin(times), end(times));
+  
+  int time = 18000;
+  int max = 0;
+  
+  i = 0;
+  while (time - times[i] >= 0 && i < n) {
+    time -= times[i];
+    ++max;
+    ++i;
+  }
+  
+  cout << max << "\n";
   
   return 0;
 }
