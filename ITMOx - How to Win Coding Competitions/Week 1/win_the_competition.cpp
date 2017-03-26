398c8565bbd832f99a9a36afd391b024d2b98333
shahenkosyan <shahen.qosyan@gmail.com>
2017-03-26 23:34:39 +0400

    Win The Competition Init

---
+++ ITMOx - How to Win Coding Competitions/Week 1/win_the_competition.cpp
@@ -0,0 +1,16 @@
+#ifdef JUDGE
+  #include <fstream>
+  std::ifstream cin("input.txt");
+  std::ofstream cout("output.txt");
+#else
+  #include <iostream>
+  using std::cin;
+  using std::cout;
+#endif
+
+#include <vector>
+
+int main() {
+  
+  return 0;
+}
