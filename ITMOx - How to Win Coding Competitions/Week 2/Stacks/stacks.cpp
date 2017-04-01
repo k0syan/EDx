7a3b2c4b3e9cbe28a7fc350713a8f25258dc86e7
shahenkosyan <shahen.qosyan@gmail.com>
2017-04-01 23:49:17 +0400

    Initialize Week 2 stacks.cpp

---
+++ ITMOx - How to Win Coding Competitions/Week 2/Stacks/stacks.cpp
@@ -0,0 +1,20 @@
+#ifdef JUDGE
+  #include <fstream>
+  std::ifstream cin("input.txt");
+  std::ofstream cout("output.txt");
+#else
+  #include <iostream>
+  using std::cin;
+  using std::cout;
+#endif
+
+
+#include <string>
+#include <stack>
+
+typedef long long ll;
+
+int main() {
+  
+  return 0;
+}
