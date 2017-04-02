767b4e0127f0cef845aa5a5b613e9645f946ae50
shahenkosyan <shahen.qosyan@gmail.com>
2017-04-02 21:15:39 +0400

    bracket_sequence.cpp init

---
+++ ITMOx - How to Win Coding Competitions/Week 2/Bracket Sequence/bracket_sequence.cpp
@@ -0,0 +1,17 @@
+#ifdef JUDGE
+  #include <fstream>
+  std::ifstream cin("input.txt");
+  std::ofstream cout("output.txt");
+#else
+  #include <iostream>
+  using std::cin;
+  using std::cout;
+#endif
+
+#include <string>
+#include <stack>
+
+int main() {
+  
+  return 0;
+}
