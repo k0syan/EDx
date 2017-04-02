4742b60c5c3dadc1e7b07d7fdf1b88d9f8569be4
shahenkosyan <shahen.qosyan@gmail.com>
2017-04-02 16:10:03 +0400

    Stacks input implement

--- ITMOx - How to Win Coding Competitions/Week 2/Snowman/snowman.cpp
+++ ITMOx - How to Win Coding Competitions/Week 2/Snowman/snowman.cpp
@@ -8,12 +8,31 @@
   using std::cout;
 #endif
 
-#include <string>
 #include <stack>
+#include <vector>
 
 typedef long long ll;
 
 int main() {
+  int n;
+  cin >> n;
+  std::vector<std::stack<int>> stacks;
+  std::stack<int> s;
+  s.push(0);
+  stacks.push_back(s);
+  
+  for (int i = 0; i < n; ++i) {
+    int index, action;
+    cin >> index >> action;
+    std::stack<int> tmp = stacks[index];
+    if (action == 0) {
+      tmp.pop();
+    } else {
+      tmp.push(action);
+    }
+    
+    stacks.push_back(tmp);
+  }
   
   return 0;
 }
