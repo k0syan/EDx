c201ab62195da850fcb3db8e6601a0b83140a087
shahenkosyan <shahen.qosyan@gmail.com>
2017-04-02 16:27:19 +0400

    Wrong Answer, test 17

--- ITMOx - How to Win Coding Competitions/Week 2/Snowman/snowman.cpp
+++ ITMOx - How to Win Coding Competitions/Week 2/Snowman/snowman.cpp
@@ -16,23 +16,33 @@ typedef long long ll;
 int main() {
   int n;
   cin >> n;
-  std::vector<std::stack<int>> stacks;
+  std::vector<std::pair<std::stack<int>, int>> actions;
   std::stack<int> s;
   s.push(0);
-  stacks.push_back(s);
+  std::pair<std::stack<int>, int> p(s, 0);
+  actions.push_back(p);
   
   for (int i = 0; i < n; ++i) {
     int index, action;
     cin >> index >> action;
-    std::stack<int> tmp = stacks[index];
+    std::stack<int> tmp = actions[index].first;
+    int weight = actions[index].second;
     if (action == 0) {
+      weight -= tmp.top();
       tmp.pop();
     } else {
       tmp.push(action);
+      weight += action;
     }
-    
-    stacks.push_back(tmp);
+    std::pair<std::stack<int>, int> p(tmp, weight);
+    actions.push_back(p);
   }
+  
+  int total = 0;
+  for (int i = 0; i <= n; ++i) {
+    total += actions[i].second;
+  }
+  cout << total << '\n';
   
   return 0;
 }
