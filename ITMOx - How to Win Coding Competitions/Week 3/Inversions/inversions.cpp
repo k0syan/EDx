409025d57cd779531eb0d26741bf9388d9d1f614
shahenkosyan <shahen.qosyan@gmail.com>
2017-04-07 19:10:35 +0400

    Inversions Count Time Limit Exceeded

--- ITMOx - How to Win Coding Competitions/Week 3/Inversions/inversions.cpp
+++ ITMOx - How to Win Coding Competitions/Week 3/Inversions/inversions.cpp
@@ -8,7 +8,67 @@
   using std::cout;
 #endif
 
+#include <vector>
+using namespace std;
+unsigned long long inversions_count = 0;
+
+vector<int> merge(vector<int> first_array, vector<int> second_array) {
+  vector<int> merged;
+
+  int j = 0;
+  int i = 0;
+  for (int k = 0; k < (int) (first_array.size() + second_array.size()); k++) {
+    if (i < (int) first_array.size()) {
+      if (j < (int) second_array.size()) {
+        if (first_array[i] <= second_array[j]) {
+          merged.push_back(first_array[i]);
+          i++;
+        } else {
+          inversions_count += first_array.size() - i;
+          merged.push_back(second_array[j]);
+          j++;
+        }
+      } else {
+        merged.push_back(first_array[i]);
+        i++;
+      }
+    } else {
+      merged.push_back(second_array[j]);
+      j++;
+    }
+  }
+
+  return merged;
+}
+
+vector<int> merge_sort(vector<int> numbers, int start, int end) {
+  if (end == start) {
+    return vector<int>{numbers[start]};
+  }
+
+  if (end - start < 2) {
+    vector<int> first_array = {numbers[start]};
+    vector<int> second_array = {numbers[end]};
+    return merge(first_array, second_array);
+  }
+
+  return merge(merge_sort(numbers, start, (end + start) / 2), merge_sort(numbers, (end + start) / 2 + 1, end));
+}
+
+
 int main() {
-  
+  int n;
+  cin >> n;
+  vector<int> numbers;
+  for (int i = 0; i < n; ++i) {
+    int tmp;
+    cin >> tmp;
+    numbers.push_back(tmp);
+  }
+
+  vector<int> merged = merge_sort(numbers, 0, (int) numbers.size() - 1);
+
+  cout << inversions_count << '\n';
+
   return 0;
 }
