9b03115213e69b4c9cf9ea9ce5dd67764e38ed11
Shahen Kosyan <k0syan@users.noreply.github.com>
2017-06-05 23:21:42 +0400

    Update generate_tests.cpp

--- ITMOx - How to Win Coding Competitions/Week 1/Generate Tests/generate_tests.cpp
+++ ITMOx - How to Win Coding Competitions/Week 1/Generate Tests/generate_tests.cpp
@@ -14,7 +14,6 @@ typedef unsigned long long ull;
 int main() {
   ull k;
   cin >> k;
-  
   std::vector<ull> highly_composite_numbers = {
     1, 2, 4, 6, 12, 24, 36, 48, 60, 
     120, 180, 240, 360, 720, 840, 1260, 
@@ -25,8 +24,8 @@ int main() {
     1081080, 1441440, 2162160, 2882880, 3603600, 
     4324320, 6486480, 7207200, 8648640
   };
-  
   ull max_index = 0;
+  
   for (ull i = 0; i < highly_composite_numbers.size(); ++i) {
     if (highly_composite_numbers[i] > k) {
       break;
